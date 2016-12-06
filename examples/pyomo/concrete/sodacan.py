1ec33b4edb062d2f7bea74617252d0d85579e8af
William Hart <whart222@gmail.com>
2016-12-06 05:58:34 -0500

    Updating example
    Including the solution process.

--- examples/pyomo/concrete/sodacan.py
+++ examples/pyomo/concrete/sodacan.py
@@ -1,13 +1,20 @@
 # sodacan.py
+
 from pyomo.environ import *
 from math import pi
 
 M = ConcreteModel()
 M.r = Var(bounds=(0,None))
 M.h = Var(bounds=(0,None))
-M.o = Objective(expr=\
-        2*pi*M.r*(M.r + M.h))
-M.c = Constraint(expr=\
-        pi*M.h*M.r**2 == 355)
+M.o = Objective(expr= 2*pi*M.r*(M.r + M.h))
+M.c = Constraint(expr= pi*M.h*M.r**2 == 355)
 
-model = M
+solver = SolverFactory('ipopt')
+
+status = solver.solve(M)
+
+print("Status = %s" % status.solver.termination_condition)
+
+print("%s = %f" % (M.r, value(M.r)))
+print("%s = %f" % (M.h, value(M.h)))
+print("Objective = %f" % value(M.o))
